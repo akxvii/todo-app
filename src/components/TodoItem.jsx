6660137d8b2d32c544ef97dcb9d81d18acf7df81
akxvii <akhil03.x@gmail.com>
2025-12-11 08:34:24 +0530

    Added TodoItem file

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import { MdDelete } from "react-icons/md";
+
+function TodoItem({ todoName, todoDate, onDeleteClick }) {
+  return (
+    <div classNameName="container">
+      <div classNameName="row kg-row">
+        <div classNameName="col-6">{todoName}</div>
+        <div classNameName="col-4">{todoDate}</div>
+        <div classNameName="col-2">
+          <button
+            type="button"
+            classNameName="btn btn-danger kg-Button"
+            onClick={() => onDeleteClick(todoName)}
+          >
+            <MdDelete />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
